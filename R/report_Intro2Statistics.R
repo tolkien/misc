3a0be4168b8d2be424ed3011a28e363d5b5a4552
Yong-iL Joh <jsjjsmith888@gmail.com>
2019-06-04 04:05:17 +0900

    update at 2019/6/4
    Signed-off-by: Yong-iL Joh <jsjjsmith888@gmail.com>

--- R/report_Intro2Statistics.R
+++ R/report_Intro2Statistics.R
@@ -2,21 +2,23 @@
 high_temp = c(29,29,34,35,35,31,32,34,38,34,33,31,31,30,34,35,
               34,32,32,29,28,30,29,31,29,28,30,29,29,27,28)
 #names(high_temp) = c(1:31)
-#jpeg("p66.jpg")
+jpeg("p66_hist.jpg")
 hist(high_temp, main="high temperature")
+dev.off()
+jpeg("p66_dot.jpg")
 dotchart(high_temp, main="high temperature of August")
+dev.off()
 stem(high_temp)
-#dev.off()
 
 #p104, Q19 두그룹 모평균 비교검정
 player_A = c(198,119,174,235,134,192,124,241,158,176)
 player_B = c(196,159,162,178,188,169,173,183,177,152)
 # H0: u_a - u_b = D_0, H1: u_a - u_b != D_0
-shapiro.test(player_A)
-shapiro.test(player_B)
-var.test(player_A, player_B)
+#shapiro.test(player_A)
+#shapiro.test(player_B)
+var.test(player_A, player_B, alternative = c("greater"))
 t.test(player_A, player_B, alternative = c("two.sided"),
-       var.equal = T)
+       var.equal = F)
 
 # by manual
 X_A = mean(player_A)
@@ -25,7 +27,14 @@ n_A = length(player_A)
 n_B = length(player_B)
 S_A = var(player_A)
 S_B = var(player_B)
-T = (X_A - X_B)
+F = S_A/S_B
+F_a2 = qf(1-0.05/2, n_A - 1, n_B - 1)
+F > F_a2
+
+S_df = (S_A/n_A + S_B/n_B)^2 / ( (S_A/n_A)^2/(n_A - 1) + (S_B/n_B)^2/(n_B - 1))
+T = (X_A - X_B)/sqrt(S_A/n_A + S_B/n_B)
+T_a2 = qt(1-0.05/2, S_df)
+T > T_a2
 
 #p229, Q11 한그룹 모평균 비교
 height=c(154,154,186,243,159,174,183,163,192,181)
@@ -50,3 +59,4 @@ koks=rbind(B1,B2,B3,B4)
 used_year =c(1.0,1.5,2.0,2.0,2.0,3.0,3.0,3.2,4.0,4.5,5.0,5.0,5.5)
 used_price=c(4.5,4.0,3.2,3.4,2.5,2.3,2.3,2.3,1.6,1.5,1.0,0.8,0.4)
 used_car=rbind(used_year,used_price)
+
