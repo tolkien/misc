ddfb3aa8c7883e674ddaf7d0685dddf821d8b153
Yong-iL Joh <jsjjsmith888@gmail.com>
2019-08-25 21:43:23 +0900

    add sample.survey, probabilities
    Signed-off-by: Yong-iL Joh <jsjjsmith888@gmail.com>

---
+++ R.2019.2/probabilities/Lecture.p2.R
@@ -0,0 +1,31 @@
+#install.packages("prob")
+library("prob")
+
+# toss coin
+tosscoin(1)
+tosscoin(3)
+
+# roll dice
+rolldie(1)
+
+# extraction
+urnsamples(x=c("a","b","c"), size=2, replace=TRUE, ordered=TRUE)
+urnsamples(x=c("a","b","c"), size=2, replace=FALSE, ordered=TRUE)
+urnsamples(x=c("a","b","c"), size=2, replace=FALSE, ordered=FALSE)
+nsamp(n=3, k=2, replace=TRUE, ordered=TRUE)
+nsamp(n=3, k=2, replace=FALSE, ordered=TRUE)
+nsamp(n=3, k=2, replace=FALSE, ordered=FALSE)
+
+# probablities of roll dice
+rolldie(1, makespace=T)
+tosscoin(1, makespace = T)
+
+# example 2-15
+S=rolldie(1, makespace = T)
+A=subset(S, X1==c(2,4,6))
+B=subset(S, X1==c(3,6))
+Prob(A)
+Prob(B)
+Prob(union(A,B))
+Prob(intersect(A,B))
+Prob(setdiff(A,B))
